72c867d2989497e0e1a018150dca5f5dff1be94b
Raj Nayan <122800487+rajnayan07@users.noreply.github.com>
2024-10-27 23:03:28 +0530

    Add files via upload

---
+++ client/tailwind.config.js
@@ -0,0 +1,11 @@
+ // tailwind.config.js
+module.exports = {
+  content: [
+    './src/**/*.{html,js,jsx,ts,tsx}',  // Adjust based on where your files are
+    './public/index.html',              // Common for React or Vue projects
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
