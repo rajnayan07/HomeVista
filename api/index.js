e4bffcb410204b8b0465c5eba56848734a5e63c1
Raj Nayan <122800487+rajnayan07@users.noreply.github.com>
2024-11-15 11:33:57 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ import userRouter from './routes/user.route.js'
 import authRouter from './routes/auth.route.js'
 import listingRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';
-
+import path from 'path';
 
 
 dotenv.config()//user.model';
@@ -37,6 +37,14 @@ app.use("/api/user",userRouter);
 app.use('/api/auth',authRouter);
 app.use('/api/listing', listingRouter);
 
+const __dirname=path.resolve()
+console.log(__dirname);
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname,'client', 'dist', 'index.html'));
+});
 
 
 app.use((err,req,res,next)=>{
